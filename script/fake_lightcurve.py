b9168a5cf5558ba983d2412755d30f351dccd3ce
Teruaki Enoto <enoto@chromy.local>
2020-10-29 08:55:18 +0900

    fake light curve

---
+++ script/fake_lightcurve.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np 
+import argparse
+import astropy.io.fits as fits
+import matplotlib.pyplot as plt
+
+__author__ = 'Teruaki Enoto'
+__version__ = '0.01'
+
+def get_parser():
+	"""
+	Creates a new argument parser.
+	"""
+	parser = argparse.ArgumentParser('fake_lightcurve.py',
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		description="""
+download nicer mkf file.
+		"""
+		)
+	version = '%(prog)s ' + __version__
+	parser.add_argument('--version', '-v', action='version', version=version,
+		help='show version of this command.')
+	parser.add_argument('--obsid', '-o', type=str, required=True, 
+		help='obsid.')	
+	parser.add_argument('--yyyymm', '-y', type=str, required=True, 
+		help='yyyymm.')		
+	parser.add_argument('--bin', '-b', type=int, required=True, 
+		help='binning time (sec) [integer].')	
+	parser.add_argument('--xmin', type=int, required=False, 
+		default=-99,help='time min')			
+	parser.add_argument('--xmax', type=int, required=False, 
+		default=-99,help='time max')	
+	parser.add_argument('--ninja', type=float, required=False, 
+		default=-99,help='Ninja count rate')								
+	return parser
+
+def wget_nicer_mkf(obsid,yyyymm):
+	yyyy_mm = yyyymm[0:4] + '_' + yyyymm[4:6]
+
+	obsid_path = 'https://heasarc.gsfc.nasa.gov/FTP/nicer/data/obs/%s/%s' % (yyyy_mm,obsid)
+	mkffile_path = '%s/auxil/ni%s.mkf.gz' % (obsid_path,obsid); 
+	mkffile = os.path.basename(mkffile_path)
+	cmd = 'wget -q -nH --no-check-certificate --cut-dirs=8 -r -l0 -c -N -np -R \'index*\' -erobots=off --retr-symlinks %s;' % mkffile_path
+	print(cmd);os.system(cmd)
+
+def plot_curve(mkffile,bin,xmin,xmax,ninja_rate):
+	"""
+	     62 TOT_XRAY_COUNT             1J
+	"""
+	print(mkffile)
+	hdu = fits.open(mkffile)
+	time_series = hdu[1].data['TIME'] - hdu[1].data['TIME'][0]
+	cnt_series = hdu[1].data['TOT_XRAY_COUNT'] 
+	err_series = np.sqrt(cnt_series)
+	module_cnt_series = np.divide(cnt_series, hdu[1].data['NUM_FPM_ON'])
+	module_err_series = np.divide(err_series, hdu[1].data['NUM_FPM_ON'])	
+
+	if xmin < 0 or xmax < 0:
+		flag_time = np.full(len(time_series), True, dtype=bool)
+	else:
+		flag_time = np.logical_and(time_series >= xmin, time_series <= xmax)
+
+	x = time_series[flag_time]
+	y = module_cnt_series[flag_time]
+	yerr = module_err_series[flag_time]
+
+	outpdf = mkffile.replace('mkf.gz','pdf')
+	fig, axes = plt.subplots(1,1,figsize=(10,7))
+	plt.errorbar(x,y,yerr=yerr, marker='', drawstyle='steps-mid') 
+	#plt.plot(time_series[flag_time],cnt_series[flag_time],'o-')
+	axes.set_xlabel('Time (sec)');
+	axes.set_ylabel('Count Rate (cps) per single NICER module')
+	axes.set_xlim(xmin,xmax)
+
+	plt.savefig(outpdf)	
+
+	cmd = 'open %s' % outpdf
+	print(cmd);os.system(cmd)
+
+	# =======
+	nicer_rate = np.nanmean(y)
+	normalization = ninja_rate / nicer_rate
+	print("nicer rate: %.1f (per module)" % nicer_rate)
+	print("ninja rate: %.1f (two GMCs)" % ninja_rate)
+	print("normalization: %.3e" % normalization)
+
+	ninja_cnt_series = normalization * y
+	ninja_err_series = np.sqrt(ninja_cnt_series)
+
+	outpdf = outpdf.replace('ni','ninjasim')
+	fig, axes = plt.subplots(1,1,figsize=(10,7))
+	#plt.errorbar(x,ninja_cnt_series,yerr=ninja_err_series, marker='', drawstyle='steps-mid') 
+	plt.plot(x,ninja_cnt_series,marker='',drawstyle='steps-mid')
+	axes.set_xlabel('Time (sec)');
+	axes.set_ylabel('NinjaSat Count Rate (cps)')
+	axes.set_xlim(xmin,xmax)
+	plt.savefig(outpdf)	
+
+	cmd = 'open %s' % outpdf
+	print(cmd);os.system(cmd)
+
+
+def main(args=None):
+	parser = get_parser()
+	args = parser.parse_args(args)
+
+	mkffile = 'ni%s.mkf.gz' % args.obsid
+	if not os.path.exists(mkffile):
+		wget_nicer_mkf(args.obsid,args.yyyymm)
+	plot_curve(mkffile,args.bin,args.xmin,args.xmax,args.ninja)
+
+if __name__=="__main__":
+	main()
+
+
---
+++ script/test_fake_lightcurve.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -f
+
+#script/fake_lightcurve.py --obsid 1103010102 --yyyymm 201707 --bin 1 --xmin 22450 --xmax 23500
+#open ni1103010102.pdf
+
+#script/fake_lightcurve.py --obsid 1020200158 --yyyymm 201901 --bin 1 --xmin 32000 --xmax 33000
+
+#script/fake_lightcurve.py --obsid 1108030110 --yyyymm 201902 --bin 1 --xmin 22800 --xmax 23000
+
+#script/fake_lightcurve.py --obsid 1108030111 --yyyymm 201902 --bin 1
+script/fake_lightcurve.py --obsid 1108030112 --yyyymm 201902 --bin 1 --xmin 55800 --xmax 57100 --ninja 1000
+#script/fake_lightcurve.py --obsid 1108030113 --yyyymm 201903 --bin 1
+
+
